f614e4f83fd1ff8c3ab04f5e90c576df74cbc187
Aminul Haque Rana <121674707+aminulhr@users.noreply.github.com>
2025-02-02 00:24:06 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 5000;
 app.get("/", (req, res) => {
-  res.send("Let explor mongoDB ");
+  res.send("Let explor mongoDB with ph level 2 web course ");
 });
 app.listen(port);
